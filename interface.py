421ff6785b5c30002831ffe3cc2e685cc0b04b8d
yungsin <37511784+yungsin@users.noreply.github.com>
2025-12-06 18:40:46 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -1520,7 +1520,7 @@ if st.session_state.current_mode is None:
     .footer-credits {{
         text-align: center;
         color: #5D5D5D;
-        font-size: 60px;
+        font-size: 55px;
         font-weight: 500;
     }}
     
