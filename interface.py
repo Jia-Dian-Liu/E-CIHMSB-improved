60d47c8c5cc51e4b28eeb109d7b8e5820346eb03
yungsin <37511784+yungsin@users.noreply.github.com>
2025-12-06 18:09:32 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -1374,7 +1374,7 @@ if st.session_state.current_mode is None:
     # ==================== 全屏選擇頁面 + 動畫卡片 ====================
     
     st.markdown("""
-    # <style>
+    """ <style>
     # html, body, [data-testid="stAppViewContainer"], .main, [data-testid="stMain"] {
     #     overflow: visible !important;
     #     max-height: 100vh !important;
@@ -1385,7 +1385,7 @@ if st.session_state.current_mode is None:
     #     overflow: visible !important;
     # }
     # </style>
-    # """, unsafe_allow_html=True)
+    # """, unsafe_allow_html=True)"""
     
     icon_secret = get_icon_base64("secret-message")
     icon_image = get_icon_base64("public-image")
