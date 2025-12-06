03283235c85e1fe317b3e10e1b5009b49776dfd3
yungsin <37511784+yungsin@users.noreply.github.com>
2025-12-06 13:31:20 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -644,7 +644,7 @@ section[data-testid="stSidebar"] button[kind="header"],
 }
 
 .welcome-title {
-    font-size: 80px;
+    font-size: 68px;
     font-weight: bold;
     letter-spacing: 0.18em;
     padding-left: 0.18em;
@@ -1427,7 +1427,7 @@ if st.session_state.current_mode is None:
     }}
     
     .welcome-title {{
-        font-size: 80px;
+        font-size: 85px;
         font-weight: bold;
         letter-spacing: 0.18em;
         padding-left: 0.18em;
@@ -1520,7 +1520,7 @@ if st.session_state.current_mode is None:
     .footer-credits {{
         text-align: center;
         color: #5D5D5D;
-        font-size: 40px;
+        font-size: 50px;
         font-weight: 500;
     }}
     
