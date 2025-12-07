8ad4a33973aad1711d8f481aee99b23463e1b362
yungsin <37511784+yungsin@users.noreply.github.com>
2025-12-07 17:04:28 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -585,7 +585,7 @@ h3 { font-size: clamp(28px, 3vw, 36px) !important; font-weight: bold !important;
 
 [data-testid="stMain"] .stSelectbox [data-baseweb="select"] span,
 [data-testid="stMain"] .stSelectbox [data-baseweb="select"] div {
-    font-size: 16px !important;
+    font-size: 14px !important;
     font-weight: bold !important;
     color: #333 !important;
 }
