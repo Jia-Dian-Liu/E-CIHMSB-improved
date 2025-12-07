17a2cd68a29dff039edde42a3019531671c7aae5
yungsin <37511784+yungsin@users.noreply.github.com>
2025-12-07 16:36:39 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -579,8 +579,8 @@ h3 { font-size: clamp(28px, 3vw, 36px) !important; font-weight: bold !important;
 [data-testid="stMain"] .stSelectbox > div > div {
     background-color: white !important;
     border-radius: 8px !important;
-    font-size: 25px !important;
-    min-height: 55px !important;
+    font-size: 30px !important;
+    min-height: 45px !important;
     padding: 12px 16px !important;
     border: 1px solid #ccc !important;
     display: flex !important;
